4a829c07e1584c9bc6de57002c41490c2bcaf3da
Akshay Parab <parab.ak@northeastern.edu>
2023-03-02 12:07:44 -0500

    returning image data on create

--- dbMigrations/migrations/20230226230203-create-image-table.mjs
+++ dbMigrations/migrations/20230226230203-create-image-table.mjs
@@ -1,70 +1,3 @@
-// 'use strict';
-
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     /**
-//      * Add altering commands here.
-//      *
-//      * Example:
-//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-//      */
-//     const transaction = await queryInterface.sequelize.transaction();
-//     try {
-//       await queryInterface.createTable('image', {
-//         'image_id': {
-//           type: Sequelize.INTEGER,
-//           allowNull: false,
-//           primaryKey: true,
-//           autoIncrement: true,
-//         },
-//         'product_id': {
-//           type: Sequelize.INTEGER,
-//           references: {
-//             model: 'product', // Can be both a string representing the table name or a Sequelize model
-//             key: 'id'
-//           }
-//         },
-//         'file_name': {
-//           type: Sequelize.STRING,
-//           allowNull: true
-//         },
-//         'date_created': {
-//           type: 'TIMESTAMP',
-//           allowNull: false,
-//           defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-//         },
-//         's3_bucket_path': {
-//           type: Sequelize.STRING,
-//           allowNull: true
-//         }
-//       }, {
-//         uniqueKeys: {
-//           product_id_image_id_unique: {
-//             fields: ['product_id', 'image_id']
-//           }
-//         },
-//       }, {
-//         transaction
-//       });
-//       await queryInterface.addIndex('image', ['product_id'], { transaction });
-//       await transaction.commit();
-//     } catch (err) {
-//       await transaction.rollback();
-//     }
-//   },
-
-//   async down(queryInterface, Sequelize) {
-//     /**
-//      * Add reverting commands here.
-//      *
-//      * Example:
-//      * await queryInterface.dropTable('users');
-//      */
-//     await queryInterface.dropTable('image');
-//   }
-// };
-// import Sequelize from 'sequelize';
 export const up = async ({
   context: {
     sequelize,
@@ -98,7 +31,8 @@ export const up = async ({
       },
       's3_bucket_path': {
         type: DataTypes.STRING,
-        allowNull: true
+        allowNull: true,
+        unique: true
       }
     }, {
       uniqueKeys: {
