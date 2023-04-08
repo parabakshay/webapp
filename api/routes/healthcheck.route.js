b8ce6ea540124452999ef083750b3a0889fd5bbf
Akshay Parab <parab.ak@northeastern.edu>
2023-04-08 00:58:09 -0400

    checking in assignment 9 code

--- api/routes/healthcheck.route.js
+++ api/routes/healthcheck.route.js
@@ -5,15 +5,44 @@ const router = express.Router();
 const path = '/healthz';
 
 router.route('/')
-    .get(logEntryPoint, async (req, res)=>{
-      try {
-        logger.info({message: "Health Check Successful", transactionId: req.trxId, responseCode: 200});
-        res.status(200).send();
-      } catch (e) {
-        logger.info({message: "Health Check Failure", transactionId: req.trxId, responseCode: 503, error: e.message});
-        res.status(503).send();
-      }
-    });
+  .get(logEntryPoint, async (req, res) => {
+    try {
+      logger.info({
+        message: "Health Check Successful",
+        transactionId: req.trxId,
+        responseCode: 200
+      });
+      res.status(200).send();
+    } catch (e) {
+      logger.info({
+        message: "Health Check Failure",
+        transactionId: req.trxId,
+        responseCode: 503,
+        error: e.message
+      });
+      res.status(503).send();
+    }
+  });
+
+router.route('/test')
+  .get(logEntryPoint, async (req, res) => {
+    try {
+      logger.info({
+        message: "Test Health Check Successful",
+        transactionId: req.trxId,
+        responseCode: 200
+      });
+      res.status(200).send("OK, Accepted");
+    } catch (e) {
+      logger.info({
+        message: "Health Check Failure",
+        transactionId: req.trxId,
+        responseCode: 503,
+        error: e.message
+      });
+      res.status(503).send();
+    }
+  });
 
 export {
   router,
